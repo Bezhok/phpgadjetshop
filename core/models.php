7b801c2602aaf071a4407e2a4b0bdf7554d9ff74
chekistchek <chekistchek@gmail.com>
2018-06-05 16:26:07 +0300

    added count, limit, foreign columns

--- core/models.php
+++ core/models.php
@@ -11,25 +11,114 @@ namespace basemodels;
 *
 */
 class BaseModel {
-    private static $pdo;
+    private static $pdo, $query, $values;
 
-    public function __construct($values = false)
+
+    public function __construct()
     {
-        $model_name = get_class($this);
+        $this->table_name = preg_replace('#.*\\\#', '', get_class($this));
+        $this->table_name = strtolower($table_name);
         $this->table_name = strtolower($model_name);
-
-        // if ($values) add_object($values);
-        // if ( object not exist ) add_object(this->fields)
     }
 
-    private function fields_check($fields)
+    private static function table_name()
     {
-        // self::mandatory_fields();
+        $table_name = preg_replace('#.*\\\#', '', static::class);
+        $table_name = strtolower($table_name);
+
+        return $table_name;
+    }
+
+
+    public static function get_all_objects(string $column='*')
+    {
+        $table_name = self::table_name();
+        self::$query = "SELECT $column FROM $table_name";
+    }
+
+    public static function plug_pdo($pdo)
+    {
+        self::$pdo = $pdo;
+    }
+
+    public static function del_object($id)
+    {
+        $table_name = self::table_name();
+        self::$query = "DELETE FROM $table_name WHERE id = ?";
+        self::$values = $id;
+    }
+
+    public static function get_with_foreign_columns(array $columns_with_namesNvalues = []) // test dangerous sql injections
+    {
+        $table_name = self::table_name();
+        // $columns_with_namesNvalues = [
+        //     'manufacturer' => ['name', 'id']
+
+        // ];
+
+        // получаем значения для запроса
+        $keys = '';
+        $concat_tables = '';
+        foreach ($columns_with_namesNvalues as $key => $value) { // key - название импортируемое таблицы
+
+            foreach ($value as $column_name) {
+                $keys .= ", {$key}.{$column_name} AS {$key}_{$column_name}";
+            }
+
+            // {название импортируемое таблицы}.{ее id} = {название таблицы}.{название импортируемое таблицы} 
+            $concat_tables .= " LEFT OUTER JOIN $key ON {$key}.id = {$table_name}.{$key}"; 
+
+        }
+
+        self::$query = "SELECT product.* $keys FROM $table_name $concat_tables";
+
+    }
+
+    public static function make_query($LIMIT = false, $LIMIT_COUNT = false)
+    {
+        if ($LIMIT && is_numeric($LIMIT)) {
+            self::$query .= " LIMIT " . (int)$LIMIT;
+            if ($LIMIT_COUNT && is_numeric($LIMIT_COUNT)) self::$query .= ", " . (int)$LIMIT_COUNT;
+        }
+        
+        if (empty(self::$values)) self::$values = [];
+
+        try { // выполняем запрос
+            $statement = self::$pdo->prepare(self::$query);
+            $statement->execute(self::$values);
+
+            return $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            echo 'Такой записи нет или указан неверный идентификатор';
+            echo $e->getMessage();
+        }
+    }
+
+    public static function get_countNmake_query()
+    {
+        $table_name = self::table_name();
+        $query = "SELECT COUNT(id) FROM $table_name";
+
+        try { // выполняем запрос
+            $statement = self::$pdo->prepare($query);
+            $statement->execute();
+
+            return $statement->fetchAll()[0][0]; // получаем значение
+
+        } catch (PDOException $e) {
+            echo 'Такой записи нет или указан неверный идентификатор';
+            echo $e->getMessage();
+        }
+    }
+
+    private function fields_check(array $fields)
+    {
         if ( !array_diff(array_keys($fields), $this->mandatory_fields()) ) $this->fields = $fields;
         else false;
     }
 
-    public function add_object($fields) 
+    public function add_object(array $fields) 
     {
         // получаем значения и ключи списка и преобразуем в sql запрос
         if ($this->fields_check($fields)) { // проверяем
@@ -55,60 +144,13 @@ class BaseModel {
         } else {
             echo 'Неправильно введены данные';
         }
-    
 
     }
-
-    public static function get_all_objects()
-    {
-        $table_name = preg_replace('#.*\\\#', '', static::class);
-        $table_name = strtolower($table_name);
-
-        $query = "SELECT * FROM $table_name";
-        
-        return self::$pdo->query($query)->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public static function plug_pdo($pdo)
-    {
-        self::$pdo = $pdo;
-    }
-
-    public static function del_object($id)
-    {
-        $table_name = strtolower(static::class); // получаем имя вызвывшего класса, узнаем таблицу в бд
-
-        $query = "DELETE FROM $table_name WHERE id = ?";
-
-        try { // выполняем запрос
-            $statement = self::$pdo->prepare($query);
-            $statement->execute([$id]);
-            echo $query;
-        } catch (PDOException $e) {
-            echo 'Такой записи нет или указан неверный идентификатор';
-            echo $e->getMessage();
-        }
-    }
-
-
-
 
     public function edit_object($fields) // не работает 
     {
-
         $query = "UPDATE $this->table_name SET ";
-
-        try { // выполняем запрос
-            $statement = self::$pdo->prepare($query);
-            $statement->execute([$id]);
-            echo $query;
-        } catch (PDOException $e) {
-            echo 'Такой записи нет или указан неверный идентификатор';
-            echo $e->getMessage();
-        }
     }
 
-
-
-
 }
+//git commit -m "added count, limit, foreign columns"
