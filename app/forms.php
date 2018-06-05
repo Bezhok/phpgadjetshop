c4f6b08b9279340f8301d44f8de41686a5b0aed6
chekistchek <chekistchek@gmail.com>
2018-06-05 20:19:59 +0300

    edited for twig

--- app/forms.php
+++ app/forms.php
@@ -5,33 +5,40 @@ namespace forms;
 
 function price_form() // создаем инпуты для фильтрации по ценам
 { 
+    $form = '';
 
-    if (isset($_REQUEST['min'])) $min = $_REQUEST['min'];
+    if (isset($_REQUEST['min'])) $min = htmlspecialchars($_REQUEST['min']);
     else $min = '';
-    echo  "<input class='input_area' type='number' name='min' autocomplete='off' placeholder='от' value='{$min}'>";
+    $form .= "<input class='input_area' type='number' name='min' autocomplete='off' placeholder='от' value='{$min}'>";
 
-    if (isset($_REQUEST['max'])) $max = $_REQUEST['max'];
+    if (isset($_REQUEST['max'])) $max = htmlspecialchars($_REQUEST['max']);
     else $max = '';
-    echo  "<input class='input_area' type='number' name='max' autocomplete='off' placeholder='от' value='{$max}'>";
+    $form .=  "<input class='input_area' type='number' name='max' autocomplete='off' placeholder='от' value='{$max}'>";
+
+    return $form;
 }
 
-function equipment_type_form($items) // создаем инпуты для фильтрации по типам товаров
+function equipment_type_form($items = ['All', 'phone', 'tablet', 'name3']) // создаем инпуты для фильтрации по типам товаров
 {
     isset($_REQUEST['equipment_type']) ? $selected = $_REQUEST['equipment_type'] : $selected = '';
-    $text = "";
+    $form = "";
     foreach ($items as $value) {
         if ($value == $selected) {
             $ch = "selected";
         } else {
             $ch = "";
         }
-        $text .= "<option $ch value='$value'>$value</option>";
+        $value = htmlspecialchars($value);
+        $form .= "<option $ch value='$value'>$value</option>";
     }
-    echo $text;
+    $form = '<select name="equipment_type" class="select_list">' . $form . '</select>';
+    return $form;
 }
 
 function years_form() // создаем чекбоксы для фильтрации по годам
 {   
+    $form = '<ul id="id_years" class="checkbox">';
+
     isset($_REQUEST['years']) ? $selected = $_REQUEST['years']: $selected = '';
     for ($i = 0; $i < 5; $i++) { // последнии 5 лет
         if ($selected && in_array( (date('Y') - $i), $selected )) { // добавляем cheked, если была выбрана
@@ -39,7 +46,13 @@ function years_form() // создаем чекбоксы для фильтрац
         } else {
             $ch = "";
         }
-        echo "<li><label for='id_years_{$i}'><input  type='checkbox' name='years[]' value='", date('Y') - $i ,"' class='checkbox' id='id_years_{$i}' $ch ></label></li>";
+        $year = date('Y') - $i;
+        $form .= "<li><label for='id_years_{$i}'><input  type='checkbox' name='years[]' value='{$year}' class='checkbox' id='id_years_{$i}' $ch ></label></li>";
+        
     }
+
+    $form .= '</ul>';
+
+    return $form;
 }
 $list = ['All', 'phone', 'tablet', 'name3']; //todo
