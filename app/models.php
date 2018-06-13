50ffde83e119799f13ca71ffa739156fd90c5723
chekistchek <chekistchek@gmail.com>
2018-06-13 18:03:19 +0300

    now you have to write type of field, added foreign and urls for images(last doesnot work)

--- app/models.php
+++ app/models.php
@@ -6,50 +6,50 @@ namespace models;
 require_once CORE_DIR . '/models.php';
 
 
+/*
+Допустимые типы: varchar, text, option, image, number
+*/
+class Product extends \basemodels\BaseModel
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-class Product extends \basemodels\BaseModel {
-
-    public static function mandatory_fields() //обязательные поля
-    { 
-        return [
-            
-            'year',
-            'manufacturer',
-            'title',
-            'price',
-            'description',
-            'equipmenttype',
-            'main_image'
-
+        $this->mandatory_fields = [
+            // 'id' => ['type' => 'number'],
+            'year' => ['type' => 'number', 'verbose_name' => 'Цена'],
+            'manufacturer' => ['type' => 'option', 'foreign' => '\models\Manufacturer'],
+            'title' => ['type' => 'varchar'],
+            'price' => ['type' => 'number'],
+            'description' => ['type' => 'text'],
+            'equipmenttype' => ['type' => 'option', 'foreign' => '\models\Equipmenttype'],
+            'main_image' => ['type' => 'image', 'url' => 'media/']
         ];
     }
-
 }
 
-class Manufacturer extends \basemodels\BaseModel {
+class Manufacturer extends \basemodels\BaseModel
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-    public static function mandatory_fields() //обязательные поля
-    { 
-        return [
-            
-            'name',
-            'image'
-
+        $this->mandatory_fields = [
+            'name' => ['type' => 'varchar'],
+            'image' => ['type' => 'image', 'url' => 'media/']
         ];
     }
-
 }
 
-class Equipmenttype extends \basemodels\BaseModel {
+class Equipmenttype extends \basemodels\BaseModel
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-    public static function mandatory_fields() //обязательные поля
-    { 
-        return [
-            
-            'name',
-            'image'
-
+        $this->mandatory_fields = [
+            'name' => ['type' => 'varchar'],
+            'image' => ['type' => 'image', 'url' => 'media/']
         ];
     }
-
 }
